4d08021852d790e080f0e91ea4039949eaf518b0
Natasha Selvidge <Natasha.selvidge@gmail.com>
2020-11-24 17:02:24 -0800

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,25 @@
+require 'pry'
+class Genre
+  attr_accessor :name, :song
+  @@all=[]
 
+  def initialize(name)
+    @name=name
+    @song=song
+    @@all << self
+  end 
+
+  def self.all
+    @@all
+  end 
+  
+  def songs
+    Song.all { |song| song.genre == self}
+  end 
+
+  def artists
+    songs.map(&:artist)
+ 
+  end 
+
+end 
